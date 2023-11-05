7e01ad04faefb9eb57fd2826731045555ee80438
Daniel McCarney <daniel@binaryparadox.net>
2023-11-04 20:29:19 -0400

    telnet: macro for pub u8 consts

--- src/telnet.rs
+++ src/telnet.rs
@@ -1,65 +1,73 @@
+// Define a public u8 constant with the given name and constant value.
+macro_rules! u8_const {
+  ($name: ident, $value: expr) => {
+    pub const $name: u8 = $value;
+  };
+}
+
 /// Module containing constants for Telnet Command codes.
 pub mod op_command {
-  pub const IAC: u8 = 255;
-  pub const WILL: u8 = 251;
-  pub const WONT: u8 = 252;
-  pub const DO: u8 = 253;
-  pub const DONT: u8 = 254;
-  pub const NOP: u8 = 241;
-  pub const SB: u8 = 250;
-  pub const SE: u8 = 240;
-  pub const IS: u8 = 0;
-  pub const SEND: u8 = 1;
-  pub const GA: u8 = 249;
-  pub const EOR: u8 = 239;
+  u8_const!(IAC, 255);
+
+  u8_const!(WILL, 251);
+  u8_const!(WONT, 252);
+  u8_const!(DO, 253);
+  u8_const!(DONT, 254);
+  u8_const!(NOP, 241);
+  u8_const!(SB, 250);
+  u8_const!(SE, 240);
+  u8_const!(IS, 0);
+  u8_const!(SEND, 1);
+  u8_const!(GA, 249);
+  u8_const!(EOR, 239);
 }
 
 /// Module containing constants for Telnet Option codes.
 pub mod op_option {
-  pub const BINARY: u8 = 0;
-  pub const ECHO: u8 = 1;
-  pub const RCP: u8 = 2;
-  pub const SGA: u8 = 3;
-  pub const NAMS: u8 = 4;
-  pub const STATUS: u8 = 5;
-  pub const TM: u8 = 6;
-  pub const RCTE: u8 = 7;
-  pub const NAOL: u8 = 8;
-  pub const NAOP: u8 = 9;
-  pub const NAOCRD: u8 = 10;
-  pub const NAOHTS: u8 = 11;
-  pub const NAOHTD: u8 = 12;
-  pub const NAOFFD: u8 = 13;
-  pub const NAOVTS: u8 = 14;
-  pub const NAOVTD: u8 = 15;
-  pub const NAOLFD: u8 = 16;
-  pub const XASCII: u8 = 17;
-  pub const LOGOUT: u8 = 18;
-  pub const BM: u8 = 19;
-  pub const DET: u8 = 20;
-  pub const SUPDUP: u8 = 21;
-  pub const SUPDUPOUTPUT: u8 = 22;
-  pub const SNDLOC: u8 = 23;
-  pub const TTYPE: u8 = 24;
-  pub const EOR: u8 = 25;
-  pub const TUID: u8 = 26;
-  pub const OUTMRK: u8 = 27;
-  pub const TTYLOC: u8 = 28;
-  pub const _3270REGIME: u8 = 29;
-  pub const X3PAD: u8 = 30;
-  pub const NAWS: u8 = 31;
-  pub const TSPEED: u8 = 32;
-  pub const LFLOW: u8 = 33;
-  pub const LINEMODE: u8 = 34;
-  pub const XDISPLOC: u8 = 35;
-  pub const ENVIRON: u8 = 36;
-  pub const AUTHENTICATION: u8 = 37;
-  pub const ENCRYPT: u8 = 38;
-  pub const NEWENVIRON: u8 = 39;
-  pub const MSSP: u8 = 70;
-  pub const ZMP: u8 = 93;
-  pub const EXOPL: u8 = 255;
-  pub const MCCP2: u8 = 86;
-  pub const MCCP3: u8 = 87;
-  pub const GMCP: u8 = 201;
+  u8_const!(BINARY, 0);
+  u8_const!(ECHO, 1);
+  u8_const!(RCP, 2);
+  u8_const!(SGA, 3);
+  u8_const!(NAMS, 4);
+  u8_const!(STATUS, 5);
+  u8_const!(TM, 6);
+  u8_const!(RCTE, 7);
+  u8_const!(NAOL, 8);
+  u8_const!(NAOP, 9);
+  u8_const!(NAOCRD, 10);
+  u8_const!(NAOHTS, 11);
+  u8_const!(NAOHTD, 12);
+  u8_const!(NAOFFD, 13);
+  u8_const!(NAOVTS, 14);
+  u8_const!(NAOVTD, 15);
+  u8_const!(NAOLFD, 16);
+  u8_const!(XASCII, 17);
+  u8_const!(LOGOUT, 18);
+  u8_const!(BM, 19);
+  u8_const!(DET, 20);
+  u8_const!(SUPDUP, 21);
+  u8_const!(SUPDUPOUTPUT, 22);
+  u8_const!(SNDLOC, 23);
+  u8_const!(TTYPE, 24);
+  u8_const!(EOR, 25);
+  u8_const!(TUID, 26);
+  u8_const!(OUTMRK, 27);
+  u8_const!(TTYLOC, 28);
+  u8_const!(_3270REGIME, 29);
+  u8_const!(X3PAD, 30);
+  u8_const!(NAWS, 31);
+  u8_const!(TSPEED, 32);
+  u8_const!(LFLOW, 33);
+  u8_const!(LINEMODE, 34);
+  u8_const!(XDISPLOC, 35);
+  u8_const!(ENVIRON, 36);
+  u8_const!(AUTHENTICATION, 37);
+  u8_const!(ENCRYPT, 38);
+  u8_const!(NEWENVIRON, 39);
+  u8_const!(MSSP, 70);
+  u8_const!(ZMP, 93);
+  u8_const!(EXOPL, 255);
+  u8_const!(MCCP2, 86);
+  u8_const!(MCCP3, 87);
+  u8_const!(GMCP, 201);
 }
